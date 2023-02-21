7e3c0c61e673f3b930c086cfc281a0a15b34da59
kevin <kevin@soucheyre.org>
2023-02-21 11:46:40 +0100

    Devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'home#index'
+  root 'static_pages#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
